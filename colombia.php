be354c4c65cea51a9fe29f67a170a5a7bef0d52a
jodacame <jodacame@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-21 22:30:36 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/20-de-julio-colombia/trunk@265480 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ colombia.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: 20 de Julio Colombia
+Plugin URI:  http://www.nexxuz.com/widget-wordpress-20-de-julio-dia-de-la-independencia-de-colombia.html
+Description: 20 de Julio día de la Independencia de Colombia, agrega la bandera de nuestro país.
+Version: 1.0
+Author: Jose Daniel Canchila
+Author URI: http://nexxuz.com
+
+*/
+
+/*  Copyright 2009 Jose Daniel Canchila - nexxuz.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+ 
+*/
+
+
+
+
+// Inicia el widget
+function show_Colombia_Ind($args) {
+	$blog_url=get_bloginfo('url');
+	?>
+<div id="twitter" style="position:fixed;width:108px;height=105px;left:-5px;top:0;padding.0px;spacing:0px;opacity: 0.9;visibility:visible;z-index:99999;border:0px solid #000000">
+<?php echo '<a href="http://www.colombiaaprende.edu.co/html/home/1592/article-83837.html" target="_Blank" rel="nofollow"><img src="'.$blog_url.'/wp-content/plugins/colombia/colombia.png" title="Dia de la Independencia de Colombia"></a> '; ?> <br />
+<a href="www.nexxuz.com"></a>
+</div>
+<?
+echo $before_title.$option_header.$after_title.$before_widget;
+
+echo $after_widget;
+
+?>
+
+<?php
+}
+
+function init_Colombia_widget() {
+register_sidebar_widget("20 de Julio Colombia", "show_Colombia_Ind");
+}
+
+add_action("plugins_loaded", "init_Colombia_widget");
+
+?>
